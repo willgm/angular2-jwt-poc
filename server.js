2621e269734ef885dd302d69d1ac42802cfe7d98
William Grasel <d.rufos@gmail.com>
2016-11-07 21:12:05 -0200

    add a mock login server with jwt

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var faker = require('faker');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+
+var jwtSecret = 'fjkdlsajfoew239053/3uk';
+
+var user = {
+  username: 'will',
+  password: 'p'
+};
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(expressJwt({ secret: jwtSecret }).unless({ path: [ '/login' ]}));
+
+app.get('/random-user', function (req, res) {
+  var user = faker.helpers.userCard();
+  user.avatar = faker.image.avatar();
+  res.json(user);
+});
+
+app.post('/login', authenticate, function (req, res) {
+  var token = jwt.sign({
+    username: user.username
+  }, jwtSecret);
+  res.send({
+    token: token,
+    user: user
+  });
+});
+
+app.get('/me', function (req, res) {
+  res.send(req.user);
+});
+
+app.listen(3000, function () {
+  console.log('App listening on localhost:3000');
+});
+
+// UTIL FUNCTIONS
+
+function authenticate(req, res, next) {
+  var body = req.body;
+  if (!body.username || !body.password) {
+    res.status(400).end('Must provide username or password');
+  }
+  if (body.username !== user.username || body.password !== user.password) {
+    res.status(401).end('Username or password incorrect');
+  }
+  next();
+}
